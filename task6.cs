f78abfa9015b1efe58555fe2357989d47d70e9ab
d3n1zws <danizeh@code.edu.az>
2026-08-26 18:01:49 +0400

    added task6

---
+++ task6.cs
@@ -0,0 +1,16 @@
+using System.Collections.Specialized;
+
+string word = Console.ReadLine();
+OnlyLetter(word);
+void OnlyLetter(string word)
+{
+    foreach (char c in word)
+    {
+        if (!char.IsLetter(c))
+        {
+            Console.WriteLine("ancaq herfden ibaret deyil");
+            return;
+        }
+    }
+    Console.WriteLine("ancaq herfden ibaretdir");
+}
